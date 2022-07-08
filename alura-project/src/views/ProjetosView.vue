8514ed1db1258f06152d956cbb0b43ca8245072e
HenriqueTrajano <henriquetrajanoguerra@gmail.com>
2022-07-08 09:53:45 -0300

    ssad

--- alura-project/src/views/ProjetosView.vue
+++ alura-project/src/views/ProjetosView.vue
@@ -2,19 +2,6 @@
     <section class="projetos">
         <h1 class="title">Projetos</h1>
         <!-- @submit.prevent ==> Ja manda aquela evitada no comportamento padrao que eh carregar a pag -->
-        <form @submit.prevent="salvarProjeto">
-            <div class="field">
-                <label for="nome-projeto" class="label">
-                    NOME DO PROJETO
-                </label>
-                <input type="text" class="input" v-model="nomeProjeto" id="nome-projeto">
-            </div>
-            <div class="field">
-                <button class="button" type="submit">
-                    Salvar
-                </button>
-            </div>
-        </form>
         <table class="table is-fullwidth">
             <thead>
                 <tr>
@@ -42,26 +29,13 @@
 
 <script lang="ts">
 import { computed, defineComponent } from "vue";
-import IProjeto from "@/interfaces/IProjeto";
 import { useStore } from "@/store";
 
 export default defineComponent({
     name: 'ProjetosView',
-    data () {
-        return {
-           nomeProjeto: ''
-        }
-    },
-    methods: {
-        salvarProjeto() {
-            this.store.commit('ADICIONA_PROJETO', this.nomeProjeto);
-            this.nomeProjeto = '';
-        }
-    }, 
     setup () {
         const store = useStore();
         return {
-            store,
             projetos: computed(() => store.state.projetos)
         }
     }
---
+++ alura-project/src/views/projects/FormularioMain.vue
@@ -0,0 +1,52 @@
+<template>
+    <section class="projetos">
+        <h1 class="title">Projetos</h1>
+        <!-- @submit.prevent ==> Ja manda aquela evitada no comportamento padrao que eh carregar a pag -->
+        <form @submit.prevent="salvarProjeto">
+            <div class="field">
+                <label for="nome-projeto" class="label">
+                    NOME DO PROJETO
+                </label>
+                <input type="text" class="input" v-model="nomeProjeto" id="nome-projeto">
+            </div>
+            <div class="field">
+                <button class="button" type="submit">
+                    Salvar
+                </button>
+            </div>
+        </form>
+    </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { useStore } from "@/store";
+
+export default defineComponent({
+    name: 'FormularioMain',
+    data () {
+        return {
+           nomeProjeto: ''
+        }
+    },
+    methods: {
+        salvarProjeto() {
+            this.store.commit('ADICIONA_PROJETO', this.nomeProjeto);
+            this.nomeProjeto = '';
+            this.$router.push('/projetos');
+        }
+    }, 
+    setup () {
+        const store = useStore();
+        return {
+            store
+        }
+    }
+})
+</script>
+
+<style scoped>
+.projetos {
+    padding: 1.25rem;
+}
+</style>
